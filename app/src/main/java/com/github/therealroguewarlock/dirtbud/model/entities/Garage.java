af717f0a6792126a453096237f4a2f953f622729
Young <andreasyoungandersen@gmail.com>
2022-03-28 21:12:50 +0200

    cleanup

--- app/src/main/java/com/github/therealroguewarlock/dirtbud/model/entities/Garage.java
+++ app/src/main/java/com/github/therealroguewarlock/dirtbud/model/entities/Garage.java
@@ -1,21 +1,39 @@
 package com.github.therealroguewarlock.dirtbud.model.entities;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
 import com.github.therealroguewarlock.dirtbud.model.entities.dirtbike.DirtBike;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity(tableName = "garage_table")
 public class Garage {
+	@PrimaryKey(autoGenerate = true)
+	private int id;
 	private final int maxAmountOfDirtBikes;
-	private List<DirtBike> dirtBikes;
+	private final List<DirtBike> dirtBikes;
 
 	public Garage(int maxAmountOfBikes) {
 		this.maxAmountOfDirtBikes = maxAmountOfBikes;
 		dirtBikes = new ArrayList<>();
 	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
 	public int getMaxAmountOfDirtBikes() {
 		return maxAmountOfDirtBikes;
+	}
+
+	public List<DirtBike> getDirtBikes() {
+		return dirtBikes;
 	}
 
 	public int getAmountOfDirtBikes() {
