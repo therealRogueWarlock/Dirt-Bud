a98fcfd03cfb3ef2041c9a91771796eab4f3c48a
Young <andreasyoungandersen@gmail.com>
2022-03-31 13:58:57 +0200

    Minor changes

--- app/src/main/java/com/github/therealroguewarlock/dirtbud/ui/garage/DirtBikeListAdapter.java
+++ app/src/main/java/com/github/therealroguewarlock/dirtbud/ui/garage/DirtBikeListAdapter.java
@@ -13,13 +13,13 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.github.therealroguewarlock.dirtbud.R;
 import com.github.therealroguewarlock.dirtbud.model.entities.dirtbike.DirtBike;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class DirtBikeListAdapter extends RecyclerView.Adapter<DirtBikeListAdapter.ViewHolder> {
-	private final ArrayList<DirtBike> dirtBikes;
+	private final List<DirtBike> dirtBikeList;
 
-	public DirtBikeListAdapter(ArrayList<DirtBike> dirtBikes) {
-		this.dirtBikes = dirtBikes;
+	public DirtBikeListAdapter(List<DirtBike> dirtBikeList) {
+		this.dirtBikeList = dirtBikeList;
 	}
 
 	@NonNull
@@ -32,15 +32,15 @@ public class DirtBikeListAdapter extends RecyclerView.Adapter<DirtBikeListAdapte
 
 	@Override
 	public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-		holder.textDirtBikeBrand.setText(dirtBikes.get(position).getBrand());
-		holder.textDirtBikeDisplacement.setText(String.valueOf(dirtBikes.get(position).getDisplacement()));
+		holder.textDirtBikeBrand.setText(dirtBikeList.get(position).getBrand());
+		holder.textDirtBikeDisplacement.setText(String.valueOf(dirtBikeList.get(position).getDisplacement()));
 		holder.imgDirtBike.setBackgroundColor(funColor(position));
 	}
 
 	private int funColor(int seed) {
 		return Color.argb(
-				(seed * dirtBikes.size() * 20) % 100,
-				(seed * 32 + dirtBikes.size() * dirtBikes.size()) % 255,
+				(seed * dirtBikeList.size() * 20) % 100,
+				(seed * 32 + dirtBikeList.size() * dirtBikeList.size()) % 255,
 				(((seed % 4) == 0) ? 18 : (((seed % 3) == 0) ? seed : ((32 * 48) + 20))) % 255,
 				(seed * 196) % 255
 		);
@@ -48,7 +48,7 @@ public class DirtBikeListAdapter extends RecyclerView.Adapter<DirtBikeListAdapte
 
 	@Override
 	public int getItemCount() {
-		return dirtBikes.size();
+		return dirtBikeList.size();
 	}
 
 	public class ViewHolder extends RecyclerView.ViewHolder {
