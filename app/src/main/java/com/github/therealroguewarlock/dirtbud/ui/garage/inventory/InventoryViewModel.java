010d18980dd3eca7acb29017d99744834eb699b4
SanderKirchert <Sanders@post.cybercity.dk>
2022-03-31 15:15:34 +0200

    extends AndroidViewModel minor fix

--- app/src/main/java/com/github/therealroguewarlock/dirtbud/ui/garage/inventory/InventoryViewModel.java
+++ app/src/main/java/com/github/therealroguewarlock/dirtbud/ui/garage/inventory/InventoryViewModel.java
@@ -1,16 +1,20 @@
 package com.github.therealroguewarlock.dirtbud.ui.garage.inventory;
 
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.ViewModel;
 
 import com.github.therealroguewarlock.dirtbud.model.GarageModel;
 import com.github.therealroguewarlock.dirtbud.model.impl.GarageModelImpl;
 
-public class InventoryViewModel extends ViewModel {
+public class InventoryViewModel extends AndroidViewModel {
 
     private final GarageModel garageModel;
 
-    public InventoryViewModel() {
-        garageModel = new GarageModelImpl();
+    public InventoryViewModel(Application application) {
+        super(application);
+        garageModel = new GarageModelImpl(application);
 
     }
 
