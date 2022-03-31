e5bc60729df24b9798183a02ff2fed2d57b2feaa
Young <andreasyoungandersen@gmail.com>
2022-03-31 16:54:23 +0200

    Ready to test, if DB binding works

---
+++ app/src/main/java/com/github/therealroguewarlock/dirtbud/model/entities/dirtbike/DirtBikePartCrossRef.java
@@ -0,0 +1,10 @@
+package com.github.therealroguewarlock.dirtbud.model.entities.dirtbike;
+
+import androidx.room.Entity;
+
+@Entity(primaryKeys = {"dirtBikeId", "partId"})
+public class DirtBikePartCrossRef {
+	public int dirtBikeId;
+	public int partId;
+	public int amount;
+}
