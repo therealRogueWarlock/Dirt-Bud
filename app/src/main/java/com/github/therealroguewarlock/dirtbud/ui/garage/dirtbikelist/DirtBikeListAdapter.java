41c4e47b782b43be9c3c67ccf4f41a1e037f8140
Young <andreasyoungandersen@gmail.com>
2022-03-31 15:39:47 +0200

    More Green in the mix :D

--- app/src/main/java/com/github/therealroguewarlock/dirtbud/ui/garage/dirtbikelist/DirtBikeListAdapter.java
+++ app/src/main/java/com/github/therealroguewarlock/dirtbud/ui/garage/dirtbikelist/DirtBikeListAdapter.java
@@ -30,13 +30,20 @@ public class DirtBikeListAdapter extends RecyclerView.Adapter<DirtBikeListAdapte
 
 	@Override
 	public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+		// Brand
+		holder.headerDirtBikeBrand.setText(R.string.card_info_brand);
 		holder.textDirtBikeBrand.setText(dirtBikeList.get(position).getBrand());
+
+		// Displacement
+		holder.headerDirtBikeDisplacement.setText(R.string.card_info_displacement);
 		holder.textDirtBikeDisplacement.setText(String.valueOf(dirtBikeList.get(position).getDisplacement()));
+
+		// Image
 		holder.imgDirtBike.setBackgroundColor(funColor(position));
 	}
 
 	private int funColor(int seed) {
-		return Color.argb((seed * dirtBikeList.size() * 20) % 100, (seed * 32 + dirtBikeList.size() * dirtBikeList.size()) % 255, (((seed % 4) == 0) ? 18 : (((seed % 3) == 0) ? seed : ((32 * 48) + 20))) % 255, (seed * 196) % 255);
+		return Color.argb((seed * dirtBikeList.size() * 20) % 100, (seed * 32 + dirtBikeList.size() * dirtBikeList.size()) % 255, (((seed % 4) == 0) ? 18 : (((seed % 3) == 0) ? seed : ((32 * seed) + 20))) % 255, (seed * 196) % 255);
 	}
 
 	@Override
@@ -52,14 +59,16 @@ public class DirtBikeListAdapter extends RecyclerView.Adapter<DirtBikeListAdapte
 	}
 
 	public class ViewHolder extends RecyclerView.ViewHolder {
+		private final TextView headerDirtBikeBrand, headerDirtBikeDisplacement;
 		private final TextView textDirtBikeBrand, textDirtBikeDisplacement;
 		private final ImageView imgDirtBike;
 
 		public ViewHolder(@NonNull View itemView) {
 			super(itemView);
-
-			textDirtBikeBrand = itemView.findViewById(R.id.brand);
-			textDirtBikeDisplacement = itemView.findViewById(R.id.displacement);
+			headerDirtBikeBrand = itemView.findViewById(R.id.header_one);
+			headerDirtBikeDisplacement = itemView.findViewById(R.id.header_two);
+			textDirtBikeBrand = itemView.findViewById(R.id.text_box_one);
+			textDirtBikeDisplacement = itemView.findViewById(R.id.text_box_two);
 			imgDirtBike = itemView.findViewById(R.id.img_dirt_bike);
 		}
 	}
