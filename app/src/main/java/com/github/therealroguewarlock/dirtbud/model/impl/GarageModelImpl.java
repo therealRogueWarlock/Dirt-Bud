2a18438818ffc4cbe722f68bae1bd1e73b2e0f81
Young <andreasyoungandersen@gmail.com>
2022-04-10 13:31:40 +0200

    Imports updated to match new package structure

--- app/src/main/java/com/github/therealroguewarlock/dirtbud/model/impl/GarageModelImpl.java
+++ app/src/main/java/com/github/therealroguewarlock/dirtbud/model/impl/GarageModelImpl.java
@@ -4,8 +4,8 @@ import android.content.Context;
 
 import androidx.lifecycle.LiveData;
 
-import com.github.therealroguewarlock.dirtbud.datapercistence.DirtBikeRepository;
-import com.github.therealroguewarlock.dirtbud.datapercistence.PartRepository;
+import com.github.therealroguewarlock.dirtbud.data_percistence.DirtBikeRepository;
+import com.github.therealroguewarlock.dirtbud.data_percistence.PartRepository;
 import com.github.therealroguewarlock.dirtbud.model.DummyData;
 import com.github.therealroguewarlock.dirtbud.model.GarageModel;
 import com.github.therealroguewarlock.dirtbud.model.entities.dirtbike.DirtBike;
