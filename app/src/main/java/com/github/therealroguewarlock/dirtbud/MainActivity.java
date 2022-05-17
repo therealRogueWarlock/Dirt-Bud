1de7d1898b8e0ffe11075246768b6351c62c5faf
Young <andreasyoungandersen@gmail.com>
2022-05-17 21:14:32 +0200

    Navigation Drawer now shows Username and Mail for current user

--- app/src/main/java/com/github/therealroguewarlock/dirtbud/MainActivity.java
+++ app/src/main/java/com/github/therealroguewarlock/dirtbud/MainActivity.java
@@ -1,79 +1,96 @@
 package com.github.therealroguewarlock.dirtbud;
 
-import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
-import com.firebase.ui.auth.IdpResponse;
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.therealroguewarlock.dirtbud.databinding.ActivityMainBinding;
+import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration mAppBarConfiguration;
-    private ActivityMainBinding binding;
+	private AppBarConfiguration mAppBarConfiguration;
+	private ActivityMainBinding binding;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+		binding = ActivityMainBinding.inflate(getLayoutInflater());
+		setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.appBarMain.toolbar);
+		setSupportActionBar(binding.appBarMain.toolbar);
 
-        Fragment navFragment = getSupportFragmentManager().getFragments().get(0);
+		Fragment navFragment = getSupportFragmentManager().getFragments().get(0);
 
+		navigationUserDetails();
 
-        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("fab fragment", "the tag:" + navFragment.getChildFragmentManager().getFragments().get(0));
-            }
-        });
+		binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Log.d("fab fragment", "the tag:" + navFragment.getChildFragmentManager().getFragments().get(0));
+			}
+		});
 
+		DrawerLayout drawer = binding.drawerLayout;
+		NavigationView navigationView = binding.navView;
+		// Passing each menu ID as a set of Ids because each
+		// menu should be considered as top level destinations.
+		mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_profile, R.id.nav_activity, R.id.nav_dirt_bike_list, R.id.nav_inventory).setOpenableLayout(drawer)
+		                                                                                                                                                       .build();
+		NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+		NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+		NavigationUI.setupWithNavController(navigationView, navController);
+	}
 
-        DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_profile, R.id.nav_activity, R.id.nav_dirt_bike_list, R.id.nav_inventory).setOpenableLayout(drawer).build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
-    }
+	private void navigationUserDetails() {
+		// Navigation Drawer Views
+		NavigationView navigationView = findViewById(R.id.nav_view);
+		View headerView = navigationView.getHeaderView(0);
+		TextView navUsername = headerView.findViewById(R.id.navUsername);
+		TextView navEmail = headerView.findViewById(R.id.navMail);
+		ImageView navImage = headerView.findViewById(R.id.navImage);
 
+		// Getting the Firebase User
+		FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+		assert user != null;
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+		// Set information for User
+		String username = user.getDisplayName();
+		String mail = user.getEmail();
+		Uri imageURI = user.getPhotoUrl();
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
-    }
+		Log.d("PFP", "Image URI: " + imageURI);
+
+		navUsername.setText(username);
+		navEmail.setText(mail);
+//		navImage.setImageURI(imageURI);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onSupportNavigateUp() {
+		NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+		return NavigationUI.navigateUp(navController, mAppBarConfiguration) || super.onSupportNavigateUp();
+	}
 }
