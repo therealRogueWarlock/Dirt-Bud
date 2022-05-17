d097e94296fa8e4674e0f63a784de656aacb504f
Young <andreasyoungandersen@gmail.com>
2022-05-17 20:23:54 +0200

    Login functionality implemented Allows for Google and Mail login

--- app/src/main/java/com/github/therealroguewarlock/dirtbud/LoginActivity.java
+++ app/src/main/java/com/github/therealroguewarlock/dirtbud/LoginActivity.java
@@ -2,24 +2,18 @@ package com.github.therealroguewarlock.dirtbud;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
-import android.widget.Toast;
 
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
 import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
-
 
 import java.util.Arrays;
 import java.util.List;
@@ -36,7 +30,10 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_firebase_ui);
+        FirebaseApp.initializeApp(getApplicationContext());
+
+        createSignInIntent();
+//        setContentView(R.layout.activity_firebase_ui);
     }
 
     public void createSignInIntent() {
@@ -44,10 +41,7 @@ public class LoginActivity extends AppCompatActivity {
         // Choose authentication providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(
                 new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.PhoneBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build(),
-                new AuthUI.IdpConfig.FacebookBuilder().build(),
-                new AuthUI.IdpConfig.TwitterBuilder().build());
+                new AuthUI.IdpConfig.GoogleBuilder().build());
 
         // Create and launch sign-in intent
         Intent signInIntent = AuthUI.getInstance()
