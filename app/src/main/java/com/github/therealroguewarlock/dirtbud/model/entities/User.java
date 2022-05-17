0b7073d1d1f1c0bcfb68b0a3c88b0944fa46b69a
Young <andreasyoungandersen@gmail.com>
2022-05-17 22:24:47 +0200

    User class Updated

--- app/src/main/java/com/github/therealroguewarlock/dirtbud/model/entities/User.java
+++ app/src/main/java/com/github/therealroguewarlock/dirtbud/model/entities/User.java
@@ -7,15 +7,18 @@ public class User {
 	private String firstName;
 	private String lastName;
 	private int age;
+	private int weight;
+	private int height;
+	private int vo2Max;
 
-	private Garage garage;
-	private Inventory inventory;
-
-	public User(String userId, String firstName, String lastName, int age) {
+	public User(String userId, String firstName, String lastName, int age, int weight, int height, int vo2Max) {
 		this.userId = userId;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.age = age;
+		this.weight = weight;
+		this.height = height;
+		this.vo2Max = vo2Max;
 	}
 
 	public String getUserId() {
@@ -26,44 +29,47 @@ public class User {
 		return firstName;
 	}
 
-	public String getLastName() {
-		return lastName;
-	}
-
-	public String getFullName() {
-		return String.format("%s %s", firstName, lastName);
-	}
-
-	public int getAge() {
-		return age;
-	}
-
-	public Garage getGarage() {
-		return garage;
-	}
-
-	public Inventory getInventory() {
-		return inventory;
-	}
-
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
 	}
 
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
 
+	public int getAge() {
+		return age;
+	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
 
-	public void setGarage(Garage garage) {
-		this.garage = garage;
+	public int getWeight() {
+		return weight;
 	}
 
-	public void setInventory(Inventory inventory) {
-		this.inventory = inventory;
+	public void setWeight(int weight) {
+		this.weight = weight;
 	}
 
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getVo2Max() {
+		return vo2Max;
+	}
+
+	public void setVo2Max(int vo2Max) {
+		this.vo2Max = vo2Max;
+	}
 }
